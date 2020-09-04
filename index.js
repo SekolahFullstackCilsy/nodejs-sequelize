51315b5fb3e669340b33fc457723943ea9f1ce9b
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-09-04 22:27:36 +0700

    add cors

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 const feedRouter = require("./routers/feed.router");
 const bookRouter = require("./routers/book.router");
 const database = require("./models");
+const cors = require("cors");
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use("/book", bookRouter);
